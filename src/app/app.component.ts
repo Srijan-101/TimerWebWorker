eb3a213201ccf3fe00a0137ab304afa23e9bd0dc
Srijankc <kcsrijan33@gmail.com>
2024-10-15 19:27:33 +0545

    Timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Timer';
+     Time:number = 0;
+     Duration!:String;
+     hours:any;
+     minutes:any;
+     seconds:any;
+     now!:Date;
+     count:number = 0;
+
+     timers:any[] = [];
+
+     ngOnInit(){
+       
+        setInterval(() => {
+          this.now = new Date()
+          this.hours = this.formatTime(this.now.getHours());
+          this.minutes = this.formatTime(this.now.getMinutes());
+          this.seconds = this.formatTime(this.now.getSeconds());
+          this.count = this.count + 1;
+          this.timers.push({count : this.count , field : `${this.hours} : ${this.minutes} : ${this.seconds}`});
+        },1000)
+     }
+
+     private formatTime(value: number): string {
+      return value < 10 ? '0' + value : value.toString();
+    }
 }
